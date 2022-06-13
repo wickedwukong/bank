ea5652cbb7c0e5092088653694d5939b248f4373
Xuemin Guan <xueminguan@Xuemins-MacBook-Pro.local>
2022-06-13 22:23:37 +0100

    A naive deposit implmentation but it passes the tests therefor checking it

--- app/src/main/kotlin/bank/Bank.kt
+++ app/src/main/kotlin/bank/Bank.kt
@@ -5,8 +5,18 @@ import java.util.*
 import java.util.Locale.US
 
 class Bank {
+    private lateinit var balance: Money
+
     fun totalBalance(): Money {
         return Money(BigDecimal.ZERO, Currency.getInstance(US))
+    }
+
+    fun deposit(customer: String, money: Money) {
+        this.balance = money
+    }
+
+    fun balanceFor(customer: String): Money {
+        return this.balance
     }
 
 }
--- app/src/test/kotlin/bank/BankTest.kt
+++ app/src/test/kotlin/bank/BankTest.kt
@@ -1,5 +1,6 @@
 package bank
 
+import java.math.BigDecimal.ONE
 import java.math.BigDecimal.ZERO
 import java.util.Currency.getInstance
 import java.util.Locale.US
@@ -10,5 +11,12 @@ class BankTest {
     @Test
     fun `Bank without customer account should have zero balance`() {
         assertEquals(Money(ZERO, getInstance(US)), Bank().totalBalance())
+    }
+
+    @Test
+    fun `A customer's first deposit is the customer's balance`() {
+        val bank = Bank()
+        bank.deposit("Alice", Money(ONE, getInstance(US)))
+        assertEquals(Money(ONE, getInstance(US)), bank.balanceFor("Alice"))
     }
 }
