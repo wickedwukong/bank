6427787ddc0a358f70e70ff5a7e0a0863a6898db
Xuemin Guan <xueminguan@Xuemins-MacBook-Pro.local>
2022-06-15 08:52:21 +0100

    refactoring tests to make it more readable

--- app/src/test/kotlin/bank/BankTest.kt
+++ app/src/test/kotlin/bank/BankTest.kt
@@ -1,10 +1,9 @@
 package bank
 
-import dev.forkhandles.result4k.Failure
-import dev.forkhandles.result4k.Success
-import dev.forkhandles.result4k.failureOrNull
-import dev.forkhandles.result4k.get
+import dev.forkhandles.result4k.*
+import java.math.BigDecimal
 import java.math.BigDecimal.*
+import java.util.*
 import java.util.Currency.getInstance
 import java.util.Locale.UK
 import java.util.Locale.US
@@ -15,201 +14,202 @@ import kotlin.test.assertNull
 class BankTest {
     @Test
     fun `should have zero total balance for a newly established Bank in the right currency`() {
-        assertEquals(Money(ZERO, getInstance(US)), Bank(getInstance(US)).totalBalance())
-        assertEquals(Money(ZERO, getInstance(UK)), Bank(getInstance(UK)).totalBalance())
+        assertEquals(usdOf(ZERO), Bank(currencyOf(US)).totalBalance())
+        assertEquals(gbpOf(ZERO), Bank(currencyOf(UK)).totalBalance())
     }
 
     @Test
     fun `should maintain the balance for a single customer's single deposit`() {
-        val bank = Bank(getInstance(US))
-        assertEquals(
-            Success(Money(ONE, getInstance(US))),
-            bank.deposit(Customer("Alice"), Money(ONE, getInstance(US)))
-        )
-
-        assertEquals(Money(ONE, getInstance(US)), bank.balanceFor(Customer("Alice")))
+        val bank = Bank(currencyOf(US))
+        
+        assertEquals(usdOf(ONE), bank.deposit(Customer("Alice"), usdOf(ONE)).valueOrNull())
+        assertEquals(usdOf(ONE), bank.balanceFor(Customer("Alice")))
     }
 
     @Test
     fun `should result in error when depositing unsupported currency`() {
-        val bank = Bank(getInstance(US))
+        val bank = Bank(currencyOf(US))
+        
         assertEquals(
-            Failure(UnSupportedCurrencyError(getInstance(US), getInstance(UK))),
-            bank.deposit(Customer("Alice"), Money(ONE, getInstance(UK)))
+            Failure(UnSupportedCurrencyError(currencyOf(US), currencyOf(UK))),
+            bank.deposit(Customer("Alice"), gbpOf(ONE))
         )
-
         assertNull(bank.balanceFor(Customer("Alice")))
     }
 
     @Test
     fun `should have no balance for an unknown customer`() {
-        val bank = Bank(getInstance(US))
+        val bank = Bank(currencyOf(US))
 
         assertNull(bank.balanceFor(Customer("Alice")))
     }
 
     @Test
     fun `should maintain the balance for a single customer's single deposit in a different currency (GBP)`() {
-        val bank = Bank(getInstance(UK))
-        bank.deposit(Customer("Alice"), Money(ONE, getInstance(UK)))
+        val bank = Bank(currencyOf(UK))
+        bank.deposit(Customer("Alice"), gbpOf(ONE))
 
-        assertEquals(Money(ONE, getInstance(UK)), bank.balanceFor(Customer("Alice")))
+        assertEquals(gbpOf(ONE), bank.balanceFor(Customer("Alice")))
     }
 
     @Test
     fun `should maintain the accumulated balance for a single customer's across multiple deposits`() {
-        val bank = Bank(getInstance(US))
-        bank.deposit(Customer("Alice"), Money(ONE, getInstance(US)))
-        bank.deposit(Customer("Alice"), Money(TEN, getInstance(US)))
+        val bank = Bank(currencyOf(US))
+        bank.deposit(Customer("Alice"), usdOf(ONE))
+        bank.deposit(Customer("Alice"), usdOf(TEN))
 
-        assertEquals(Money(valueOf(11), getInstance(US)), bank.balanceFor(Customer("Alice")))
+        assertEquals(usdOf(valueOf(11)), bank.balanceFor(Customer("Alice")))
     }
 
 
     @Test
     fun `should maintain accumulated balance for multiple customers across multiple deposits`() {
-        val bank = Bank(getInstance(US))
-        bank.deposit(Customer("Alice"), Money(ONE, getInstance(US)))
-        bank.deposit(Customer("Alice"), Money(TEN, getInstance(US)))
-        bank.deposit(Customer("Bob"), Money(TEN, getInstance(US)))
+        val bank = Bank(currencyOf(US))
+        bank.deposit(Customer("Alice"), usdOf(ONE))
+        bank.deposit(Customer("Alice"), usdOf(TEN))
+        bank.deposit(Customer("Bob"), usdOf(TEN))
 
-        assertEquals(Money(valueOf(11), getInstance(US)), bank.balanceFor(Customer("Alice")))
-        assertEquals(Money(TEN, getInstance(US)), bank.balanceFor(Customer("Bob")))
+        assertEquals(usdOf(valueOf(11)), bank.balanceFor(Customer("Alice")))
+        assertEquals(usdOf(TEN), bank.balanceFor(Customer("Bob")))
     }
 
     @Test
     fun `should maintain the total accumulated balance for the Bank's across customer deposits - single customer`() {
-        val bank = Bank(getInstance(US))
-        bank.deposit(Customer("Alice"), Money(ONE, getInstance(US)))
-        bank.deposit(Customer("Alice"), Money(TEN, getInstance(US)))
+        val bank = Bank(currencyOf(US))
+        bank.deposit(Customer("Alice"), usdOf(ONE))
+        bank.deposit(Customer("Alice"), usdOf(TEN))
 
-        assertEquals(Money(valueOf(11), getInstance(US)), bank.totalBalance())
+        assertEquals(usdOf(valueOf(11)), bank.totalBalance())
     }
 
     @Test
     fun `should maintain the total accumulated balance for the Bank's across customer deposits - multiple customers`() {
-        val bank = Bank(getInstance(US))
-        bank.deposit(Customer("Alice"), Money(ONE, getInstance(US)))
-        bank.deposit(Customer("Alice"), Money(TEN, getInstance(US)))
-        bank.deposit(Customer("Bob"), Money(TEN, getInstance(US)))
+        val bank = Bank(currencyOf(US))
+        bank.deposit(Customer("Alice"), usdOf(ONE))
+        bank.deposit(Customer("Alice"), usdOf(TEN))
+        bank.deposit(Customer("Bob"), usdOf(TEN))
 
-        assertEquals(Money(valueOf(21), getInstance(US)), bank.totalBalance())
+        assertEquals(Money(valueOf(21), currencyOf(US)), bank.totalBalance())
     }
 
     @Test
     fun `should result in error when attempting depositing zero or negative amount, customer and Bank's total balance should not be changed`() {
-        val bank = Bank(getInstance(US))
+        val bank = Bank(currencyOf(US))
         bank.deposit(Customer("Alice"), Money(ONE, bank.currency))
 
 
         assertEquals(
-            AmountHasToBePositiveError(Money(valueOf(0), getInstance(US))),
-            bank.deposit(Customer("Alice"), Money(ZERO, bank.currency)).failureOrNull()
+            AmountHasToBePositiveError(Money(valueOf(0), currencyOf(US))),
+            bank.deposit(Customer("Alice"), usdOf(ZERO)).failureOrNull()
         )
 
         assertEquals(
-            AmountHasToBePositiveError(Money(valueOf(-1), getInstance(US))),
-            bank.deposit(Customer("Alice"), Money(valueOf(-1), bank.currency)).failureOrNull()
+            AmountHasToBePositiveError(Money(valueOf(-1), currencyOf(US))),
+            bank.deposit(Customer("Alice"), usdOf(valueOf(-1))).failureOrNull()
         )
 
-        assertEquals(Money(ONE, getInstance(US)), bank.balanceFor(Customer("Alice")))
-        assertEquals(Money(ONE, getInstance(US)), bank.totalBalance())
+        assertEquals(usdOf(ONE), bank.balanceFor(Customer("Alice")))
+        assertEquals(usdOf(ONE), bank.totalBalance())
     }
 
 
     @Test
     fun `should result in error when withdrawing for an unknown customer and Bank's total balance is not changed`() {
-        val bank = Bank(getInstance(US))
+        val bank = Bank(currencyOf(US))
         assertEquals(
             UnknownCustomerError(Customer("UnknownCustomerId")),
-            bank.withdraw(Customer("UnknownCustomerId"), Money(ONE, bank.currency)).failureOrNull()
+            bank.withdraw(Customer("UnknownCustomerId"), usdOf(ONE)).failureOrNull()
         )
 
-        assertEquals(Money(ZERO, getInstance(US)), bank.totalBalance())
+        assertEquals(usdOf(ZERO), bank.totalBalance())
     }
 
     @Test
     fun `should let customer withdraw money which amount is less than existing balance and maintain customer's new balance after withdraw`() {
-        val bank = Bank(getInstance(US))
-        bank.deposit(Customer("Alice"), Money(TEN, getInstance(US)))
+        val bank = Bank(currencyOf(US))
+        bank.deposit(Customer("Alice"), usdOf(TEN))
 
-        assertEquals(Money(ONE, getInstance(US)), bank.withdraw(Customer("Alice"), Money(ONE, bank.currency)).get())
-        assertEquals(Money(valueOf(9), getInstance(US)), bank.balanceFor(Customer("Alice")))
+        assertEquals(usdOf(ONE), bank.withdraw(Customer("Alice"), usdOf(ONE)).get())
+        assertEquals(usdOf(valueOf(9)), bank.balanceFor(Customer("Alice")))
     }
+
     @Test
     fun `should let customer withdraw all of the customer's balance and the customer's balance should be zero after the withdraw`() {
-        val bank = Bank(getInstance(US))
-        bank.deposit(Customer("Alice"), Money(TEN, getInstance(US)))
+        val bank = Bank(currencyOf(US))
+        bank.deposit(Customer("Alice"), usdOf(TEN))
 
-        assertEquals(Money(TEN, getInstance(US)), bank.withdraw(Customer("Alice"), Money(TEN, bank.currency)).get())
-        assertEquals(Money(ZERO, getInstance(US)), bank.balanceFor(Customer("Alice")))
+        assertEquals(usdOf(TEN), bank.withdraw(Customer("Alice"), usdOf(TEN)).get())
+        assertEquals(usdOf(ZERO), bank.balanceFor(Customer("Alice")))
     }
 
     @Test
     fun `should result in error when customer withdraws more than existing balance and customer's balance remains unchanged`() {
-        val bank = Bank(getInstance(US))
-        bank.deposit(Customer("Alice"), Money(ONE, getInstance(US)))
+        val bank = Bank(currencyOf(US))
+        bank.deposit(Customer("Alice"), usdOf(ONE))
 
         assertEquals(
-            WithdrawExceedingBalanceError(Customer("Alice"), Money(valueOf(2), getInstance(US))),
-            bank.withdraw(Customer("Alice"), Money(valueOf(2), bank.currency)).failureOrNull()
+            WithdrawExceedingBalanceError(Customer("Alice"), usdOf(valueOf(2))),
+            bank.withdraw(Customer("Alice"), usdOf(valueOf(2))).failureOrNull()
         )
-
-        assertEquals(Money(ONE, getInstance(US)), bank.balanceFor(Customer("Alice")))
+        assertEquals(usdOf(ONE), bank.balanceFor(Customer("Alice")))
     }
 
     @Test
     fun `should result in error when withdrawing Zero or less than Zero amount Customer's balance and Bank's total balance are unchanged`() {
-        val bank = Bank(getInstance(US))
-        bank.deposit(Customer("Alice"), Money(ONE, getInstance(US)))
+        val bank = Bank(currencyOf(US))
+        bank.deposit(Customer("Alice"), usdOf(ONE))
 
         assertEquals(
-            AmountHasToBePositiveError(Money(valueOf(0), getInstance(US))),
-            bank.withdraw(Customer("Alice"), Money(ZERO, bank.currency)).failureOrNull()
+            AmountHasToBePositiveError(Money(valueOf(0), currencyOf(US))),
+            bank.withdraw(Customer("Alice"), usdOf(ZERO)).failureOrNull()
         )
-        assertEquals(Money(ONE, getInstance(US)), bank.balanceFor(Customer("Alice")))
+        assertEquals(usdOf(ONE), bank.balanceFor(Customer("Alice")))
 
         assertEquals(
-            AmountHasToBePositiveError(Money(valueOf(-1), getInstance(US))),
-            bank.withdraw(Customer("Alice"), Money(valueOf(-1), bank.currency)).failureOrNull()
+            AmountHasToBePositiveError(Money(valueOf(-1), currencyOf(US))),
+            bank.withdraw(Customer("Alice"), usdOf(valueOf(-1))).failureOrNull()
         )
 
-        assertEquals(Money(ONE, getInstance(US)), bank.balanceFor(Customer("Alice")))
-        assertEquals(Money(ONE, getInstance(US)), bank.totalBalance())
+        assertEquals(usdOf(ONE), bank.balanceFor(Customer("Alice")))
+        assertEquals(usdOf(ONE), bank.totalBalance())
     }
 
     @Test
     fun `should result in error when withdrawing unsupported currency and maintain customer's original balance and bank's total balance`() {
-        val bank = Bank(getInstance(US))
-        bank.deposit(Customer("Alice"), Money(ONE, getInstance(US)))
+        val bank = Bank(currencyOf(US))
+        bank.deposit(Customer("Alice"), usdOf(ONE))
 
         assertEquals(
-            Failure(UnSupportedCurrencyError(getInstance(US), getInstance(UK))),
-            bank.withdraw(Customer("Alice"), Money(ONE, getInstance(UK)))
+            Failure(UnSupportedCurrencyError(currencyOf(US), currencyOf(UK))),
+            bank.withdraw(Customer("Alice"), gbpOf(ONE))
         )
 
-        assertEquals(Money(ONE, getInstance(US)), bank.balanceFor(Customer("Alice")))
-        assertEquals(Money(ONE, getInstance(US)), bank.totalBalance())
+        assertEquals(usdOf(ONE), bank.balanceFor(Customer("Alice")))
+        assertEquals(usdOf(ONE), bank.totalBalance())
     }
 
     @Test
     fun `should maintain bank's total balance with various deposits and withdraws across multiple customers`() {
-        val bank = Bank(getInstance(US))
-        bank.deposit(Customer("Alice"), Money(ONE, getInstance(US)))
-        assertEquals(Money(ONE, getInstance(US)), bank.totalBalance())
+        val bank = Bank(currencyOf(US))
+        bank.deposit(Customer("Alice"), usdOf(ONE))
+        assertEquals(usdOf(ONE), bank.totalBalance())
 
-        bank.deposit(Customer("Bob"), Money(ONE, getInstance(US)))
-        assertEquals(Money(valueOf(2), getInstance(US)), bank.totalBalance())
+        bank.deposit(Customer("Bob"), usdOf(ONE))
+        assertEquals(usdOf(valueOf(2)), bank.totalBalance())
 
-        bank.deposit(Customer("Alice"), Money(ONE, getInstance(US)))
-        assertEquals(Money(valueOf(3), getInstance(US)), bank.totalBalance())
+        bank.deposit(Customer("Alice"), usdOf(ONE))
+        assertEquals(usdOf(valueOf(3)), bank.totalBalance())
 
-        bank.withdraw(Customer("Alice"), Money(ONE, getInstance(US)))
-        assertEquals(Money(valueOf(2), getInstance(US)), bank.totalBalance())
+        bank.withdraw(Customer("Alice"), usdOf(ONE))
+        assertEquals(usdOf(valueOf(2)), bank.totalBalance())
 
-        bank.withdraw(Customer("Alice"), Money(ONE, getInstance(US)))
-        assertEquals(Money(ZERO, getInstance(US)), bank.balanceFor(Customer("Alice")))
-        assertEquals(Money(ONE, getInstance(US)), bank.balanceFor(Customer("Bob")))
-        assertEquals(Money(valueOf(1), getInstance(US)), bank.totalBalance())
+        bank.withdraw(Customer("Alice"), usdOf(ONE))
+        assertEquals(usdOf(ZERO), bank.balanceFor(Customer("Alice")))
+        assertEquals(usdOf(ONE), bank.balanceFor(Customer("Bob")))
+        assertEquals(usdOf(ONE), bank.totalBalance())
     }
+
+    private fun usdOf(value: BigDecimal) = Money(value, currencyOf(US))
+    private fun gbpOf(value: BigDecimal) = Money(value, currencyOf(UK))
+    private fun currencyOf(locale: Locale) = getInstance(locale)
 }
